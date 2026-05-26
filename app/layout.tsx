4817cebb23f29bbeba92edd3f883eaa4b7588db0
Fayyad Alaa <fayyad.alaa.s@gmail.com>
2026-05-26 14:41:19 -0400

    Add Microsoft Clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -215,24 +215,35 @@ export default async function RootLayout({
         />
 
         {/* Google Analytics */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-4V7MN48JS7"
-          strategy="afterInteractive"
-        />
+<Script
+  src="https://www.googletagmanager.com/gtag/js?id=G-4V7MN48JS7"
+  strategy="afterInteractive"
+/>
 
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              window.gtag = gtag;
-              gtag('js', new Date());
-              gtag('config', 'G-4V7MN48JS7');
-            `,
-          }}
-        />
+<Script
+  id="google-analytics"
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      window.gtag = gtag;
+      gtag('js', new Date());
+      gtag('config', 'G-4V7MN48JS7');
+    `,
+  }}
+/>
+
+{/* Microsoft Clarity */}
+<Script id="clarity" strategy="afterInteractive">
+  {`
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "wx9wkae5k7");
+  `}
+</Script>
       </head>
 
       <body
